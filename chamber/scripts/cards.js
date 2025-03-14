fd0faea8b5907a1425b381c20e4c0251f47a2f46
tysonpace777 <32.tpace@gmail.com>
2025-03-14 13:53:41 -0600

    js and json finished

---
+++ chamber/scripts/cards.js
@@ -0,0 +1,57 @@
+async function loadData() {
+    try {
+      const response = await fetch('data/members.json');
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+  
+      const data = await response.json();
+  
+      displayData(data.businesses);
+  
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }
+
+function displayData(data) {
+    const container = document.getElementById('cards');
+    container.innerHTML = '';
+
+    if (Array.isArray(data)) {
+      data.forEach(business => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const name = document.createElement('h3');
+        name.textContent = business.name;
+
+        const address = document.createElement('p');
+        address.textContent = `Address: ${business.address}`;
+
+        const phone = document.createElement('p');
+        phone.textContent = `Phone: ${business.phone}`;
+
+        const website = document.createElement('a');
+        website.href = `http://${business.website}`;
+        website.textContent = `Visit Website`;
+        website.target = '_blank';
+
+        const image = document.createElement('img');
+        image.src = business.image;
+        image.alt = `${business.name} image`;
+        image.width = 150;
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        container.appendChild(card);
+      });
+    }
+  }
+
+loadData();
--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -5,3 +5,20 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+
+const display = document.querySelector('#cards')
+
+const gridButton = document.querySelector('#grid');
+
+gridButton.addEventListener('click', () => {
+    display.classList.add('grid');
+    display.classList.remove('list');
+})
+
+const listButton = document.querySelector('#list');
+
+listButton.addEventListener('click', () => {
+    display.classList.add('list');
+    display.classList.remove('grid');
+})
